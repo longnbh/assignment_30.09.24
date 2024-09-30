f0908066b3f191877c5f036e671cf97c2679337b
longnbh <hoanglongjimmy2005@gmail.com>
2024-09-30 16:03:11 +0700

    define class

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    private String yearOfBirth;
+    private String fullName;
+
+    public String getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(String yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public static void main(String[] args) {
+        Person giangVien;
+        Person sinhVien;
+        Person nhanSuKhac;
+    }
+
+
+}
