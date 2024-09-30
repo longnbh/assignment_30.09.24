c5f7a4ea7d9fea3ed70257a903060991d5cf667c
longnbh <hoanglongjimmy2005@gmail.com>
2024-09-30 16:19:04 +0700

    code function main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Person {
+class Person {
 
     private String yearOfBirth;
     private String fullName;
@@ -32,6 +32,15 @@ public class Person {
 
 }
 
+class Lecturer extends Person {
+    public Lecturer() {}
+
+    public Lecturer(String yearOfBirth, String fullName) {
+        super(yearOfBirth, fullName);
+    }
+
+}
+
 class Student extends Person {
     private String mssv;
 
@@ -48,5 +57,21 @@ class Student extends Person {
 
     public void setMssv(String mssv) {
         this.mssv = mssv;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Lecturer l1 = new Lecturer("1997", "Tran Manh Cuong");
+        Lecturer l2 = new Lecturer("2001", "Kieu Van Tuyen");
+
+        Student s1 = new Student("2005", "Nguyen Ba Hoang Long", "23020680");
+        Student s2 = new Student("2004", "Tran Van Dong", "22020719");
+
+        Person other1 = new Person("1978", "Nguyen Van A");
+        Person other2 = new Person("1985", "Tran Thi B");
+
+        //in danh sach giang vien
+        System.out.println("Danh sach giang vien:");
     }
 }
